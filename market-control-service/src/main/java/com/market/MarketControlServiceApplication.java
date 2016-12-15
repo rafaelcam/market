6a3b8dde7b3fd421c7abfdce9fe2d20b36cf7007
Unknown <jrafael_campossilva@hotmail.com>
2016-12-14 21:37:56 -0300

    Criando os projetos e configurando dependências.

---
+++ market-control-service/src/main/java/com/market/MarketControlServiceApplication.java
@@ -0,0 +1,12 @@
+package com.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketControlServiceApplication.class, args);
+	}
+}
---
+++ market-web-view/src/main/java/com/market/MarketWebViewApplication.java
@@ -0,0 +1,12 @@
+package com.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketWebViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketWebViewApplication.class, args);
+	}
+}
