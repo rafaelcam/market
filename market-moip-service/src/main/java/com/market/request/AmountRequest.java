1959fc4f0505740e10405f7f59c11e67ffda8a83
João Rafael Campos da Silva <jrafael_campossilva@hotmail.com>
2016-12-22 18:31:42 -0300

    Enviando desconto e adicional.

---
+++ market-moip-service/src/main/java/com/market/request/AmountRequest.java
@@ -0,0 +1,6 @@
+package com.market.request;
+
+public class AmountRequest {
+
+
+}
---
+++ market-moip-service/src/main/java/com/market/request/OrderRequest.java
@@ -0,0 +1,8 @@
+package com.market.request;
+
+
+public class OrderRequest extends br.com.moip.request.OrderRequest {
+
+
+
+}
